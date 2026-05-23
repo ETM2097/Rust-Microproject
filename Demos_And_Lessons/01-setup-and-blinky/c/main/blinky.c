21b3f24f6fb854ab1b9ef97dbbf2e3f5bc263d8d
Enric <enricxz@gmail.com>
2026-05-23 12:46:57 +0200

    Added first example of a blinking led in C, next will be in RUST with explanations

---
+++ Demos_And_Lessons/01-setup-and-blinky/c/main/blinky.c
@@ -0,0 +1,21 @@
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define LED_GPIO          GPIO_NUM_2
+#define BLINK_PERIOD_MS   500U
+
+void app_main(void)
+{
+    uint32_t led_level = 0U;
+
+    gpio_reset_pin(LED_GPIO);
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+
+    while (1)
+    {
+        gpio_set_level(LED_GPIO, led_level);
+        led_level = (0U == led_level) ? 1U : 0U;
+        vTaskDelay(pdMS_TO_TICKS(BLINK_PERIOD_MS));
+    }
+}
